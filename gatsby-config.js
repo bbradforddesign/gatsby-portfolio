30878bfccb2496321ccc5655b11f7cc889e9d9d2
Blake Bradford <bbradforddesign@gmail.com>
2021-01-06 22:38:23 -0600

    Gatsby and Contentful successfully connected

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BBradfordDesign`,
+    description: `2020 Blake Bradford`,
+    author: `@bbradforddesign`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +29,15 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        forceFullSync: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,11 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => {
+const AboutPage = () => {
   const data = useStaticQuery(
     graphql`
       query {
-        contentfulPortfolioPage(pageTitle: { eq: "Welcome" }) {
+        contentfulPortfolioPage(pageTitle: { eq: "About Me" }) {
           pageContent {
             raw
           }
@@ -26,11 +26,11 @@ const IndexPage = () => {
         documentToReactComponents(
           JSON.parse(data.contentfulPortfolioPage.pageContent.raw)
         )}
-      <Link to="/about/">About Me</Link> <br />
+      <Link to="/">Home</Link> <br />
       <Link to="/portfolio/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
   )
 }
 
-export default IndexPage
+export default AboutPage
