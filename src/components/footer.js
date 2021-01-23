b0094bf1b2fb4806af0894be704f055954a40f84
Blake Bradford <bbradforddesign@gmail.com>
2021-01-22 23:27:42 -0600

    Created base footer

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Footer = () => (
+  <footer class="flex justify-between inline-block p-4 mt-12">
+    <p>2021 Blake Bradford</p>
+    <ul class="flex flex-row items-center font-medium">
+      <li class="mx-3 hover:text-yellow-300">
+        <a href="/">LinkedIn</a>
+      </li>
+      <li class="mx-3 hover:text-yellow-300">
+        <a href="/">GitHub</a>
+      </li>
+      <li class="mx-3 hover:text-yellow-300">
+        <a href="/">Email</a>
+      </li>
+    </ul>
+  </footer>
+)
+
+export default Footer
