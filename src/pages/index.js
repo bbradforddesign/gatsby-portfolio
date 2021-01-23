bdd179762e13f0cc30338e54fab6a42c2b15e686
Blake Bradford <bbradforddesign@gmail.com>
2021-01-23 00:22:11 -0600

    Began page layout for index

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,21 +15,37 @@ const IndexPage = () => {
             raw
           }
           pageTitle
+          pageImages {
+            fixed {
+              src
+            }
+          }
         }
       }
     `
   )
 
+  console.log(data.contentfulSitePage.pageImages)
+
   return (
     <Layout>
       <SEO title={data.contentfulSitePage.pageTitle} />
-      {data.contentfulSitePage.pageContent.raw &&
-        documentToReactComponents(
-          JSON.parse(data.contentfulSitePage.pageContent.raw)
-        )}
-      <CTA message="Nice to Meet You!" to="/about" />
-      <Link to="/portfolio/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <div class="grid grid-cols-2 mb-auto">
+        {/** Hero Img */}
+        <div className="flex justify-center">
+          {data && (
+            <img src={data.contentfulSitePage.pageImages[0].fixed.src} />
+          )}
+        </div>
+        {/** Hero Text */}
+        <div>
+          {data &&
+            documentToReactComponents(
+              JSON.parse(data.contentfulSitePage.pageContent.raw)
+            )}
+          <CTA message="Nice to Meet You!" to="/about" />
+        </div>
+      </div>
     </Layout>
   )
 }
